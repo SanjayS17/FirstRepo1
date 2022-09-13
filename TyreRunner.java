c2377fc25abc0b9acfdd98e41be0ca1e25bf4b61
sanjay s <sanjays.xworkz@gmail.com>
2022-09-13 11:36:08 +0530

    adding in to git

---
+++ TyreRunner.java
@@ -0,0 +1,19 @@
+class TyreRunner{
+	public static void main(String[]tyre)
+	{
+		
+		System.out.println("Running main method");
+		Tyre.nonskidding();
+		Tyre.noiseResistant();
+		Tyre.capacity();
+		Tyre.powerconsumption();
+		Tyre.uniformwear();
+		Tyre.tread();
+		Tyre.shoulder();
+		Tyre.sipes();
+		Tyre.grooves();
+		Tyre.innerliner();
+		Tyre.cushioning();
+		Tyre.sidewall();
+	}	
+}
